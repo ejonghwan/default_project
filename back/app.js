a2fcc9a120b798013f4542654f1c30f355388df9
ejonghwan <sun87-1@daum.net>
2022-05-10 20:40:14 +0900

    asd

--- back/app.js
+++ back/app.js
@@ -3,6 +3,8 @@ import multer from 'multer';
 import { v4 } from 'uuid';
 import mime from 'mime-types'; //file type 지정
 import cors from 'cors'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 
 
@@ -19,6 +21,8 @@ cb: 콜백
 destionation: 어디에 저장할지 
 filename: 어떤 이름으로 저장할지
 */
+
+
 
 const upload = multer({ 
     storage, 
@@ -42,21 +46,34 @@ const PORT = 5000;
 // app.use(cors())
 app.use('/uploads', express.static('uploads')) //http://localhost:5000/uploads/ae791f20-ca35-4e95-919b-655d94791127.jpeg 이거 접근됨...이거 없음 접근안됨
 
+dotenv.config()
 
 
-app.post('/upload', upload.single("image"), (req, res) => { 
-    //post로 이미지를 보낼때 이미 req에 저장이 되어있음. upload(미들웨어)를 이용해서 저장된 이미지 가져옴 
-    // postman  body -> form-data -> key에 위에 미들웨어와 동일한 키값 image넣음
+mongoose.connect(process.env.MONGODB_URI, {
+    // useCreateIndex: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+}).then(() => {
+
+    console.log('mongodb connected')
     
-    console.log(req.file)
-    res.json(req.file);
-})
+    app.post('/upload', upload.single("image"), (req, res) => { 
+        //post로 이미지를 보낼때 이미 req에 저장이 되어있음. upload(미들웨어)를 이용해서 저장된 이미지 가져옴 
+        // postman  body -> form-data -> key에 위에 미들웨어와 동일한 키값 image넣음
+        
+        console.log(req.file)
+        res.json(req.file);
+    })
 
 
-app.get('/test', (req, res) => {
-    res.json('hehe')
-})
-  
+    app.get('/test', (req, res) => {
+        res.json('hehe')
+    })
+      
 
 
-app.listen(PORT, () => console.log('express server listening port ' + PORT))
+    app.listen(PORT, () => console.log('express server listening port ' + PORT))
+
+
+}).catch(err => console.error(err))
+
